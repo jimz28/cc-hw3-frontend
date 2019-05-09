d7193e8dfce6c5a93c44f06f6ff1498e804b5874
jimz28 <zhi.zheng@outlook.com>
2019-05-08 23:40:44 -0400

    clean up format

--- src/views/addPhoto.js
+++ src/views/addPhoto.js
@@ -2,6 +2,16 @@ import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
 import {} from '../api/photo';
 import axios from 'axios';
+import S3 from 'aws-s3';
+ 
+// const config = {
+//     bucketName: 'myBucket',
+//     // dirName: 'photos', /* optional */
+//     region: 'us-east-1',
+//     accessKeyId: 'AKIAVMUOUS7R4AFU4Y3S',
+//     secretAccessKey: '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',
+//     s3Url: 'http://s3.amazonaws.com/cloudhw3bucket/', /* optional */
+// }
 
 
 class AddPhoto extends Component {
@@ -12,14 +22,45 @@ class AddPhoto extends Component {
 
 state = {
     imageURL: null,
-    loading: false
+    loading: false,
+    status: ''
 }
 
 handleFileChange = async event => {
   if (!!event.target.files[0]) {
+
+    const file = event.target.files[0];
+    const fileName = file.name;
+    const options = {
+      headers: {
+        'Content-Type': file.type
+      }
+    };
+    console.log(event.target.files[0]);
+
     await this.setState({
-      imageURL: URL.createObjectURL(event.target.files[0]),
+      imageURL: URL.createObjectURL(file),
       loading: true
+    });
+
+    // const S3Client = new S3(config);
+    // /*  Notice that if you don't provide a dirName, the file will be automatically uploaded to the root of your bucket */
+    
+    // /* This is optional */
+    // const newFileName = fileName;
+    
+    // S3Client
+    //     .uploadFile(file, newFileName)
+    //     .then(data => console.log(data))
+    //     .catch(err => console.error(err))
+
+    axios.put(`https://ec83d6qdgc.execute-api.us-east-1.amazonaws.com/stage/upload2/${fileName}`, file, options)
+    .then((response) => {
+      console.log(response);
+      this.setState({status: 'uploaded successfully'});
+    })
+    .catch(function (error) {
+        console.log(error);
     });
   }
 };
@@ -35,6 +76,7 @@ render() {
           onChange={this.handleFileChange}
           accept=".jpg, .jpeg, .png"
         />
+        <div>{this.state.status}</div>
         <div style={{ position: 'relative' }}>
           <div style={{ position: 'absolute' }}>
             <img src={imageURL} alt="imageURL" class="rounded mx-auto d-block"/>
--- src/views/home.js
+++ src/views/home.js
@@ -3,34 +3,52 @@ import React, { Component } from 'react';
 import Gallery from "react-photo-gallery";
 import axios from 'axios';
 
+const API = 'https://hn.algolia.com/api/v1/search?query=';
+const DEFAULT_QUERY = 'redux';
+const API2 = 'https://ec83d6qdgc.execute-api.us-east-1.amazonaws.com/stage/search';
+
 
 export default class Home extends Component {
     state = {
         inputValue: '',
-        photos: null,
+        photos: [],
     };
 
     // let photoList = [];
+
+    componentDidMount() {
+        // fetch(API2 + "?q=show+me+cat", {
+        //     method: 'get',
+        //     headers: {
+        //       'Accept': 'application/json',
+        //       'Content-Type': 'application/json',
+        //     },
+        // })
+        //   .then(response => response.json())
+        //   .then(data => console.log(data));
+      }
+    
 
     handleInputChange = (event) => {
         this.setState({inputValue: event.target.value});
     }
 
     handleFormSubmit = (event) => {
-        console.log(this.state.inputValue);
-        axios.get("https://ec83d6qdgc.execute-api.us-east-1.amazonaws.com/stage/search", {
+        // console.log(this.state.inputValue);
+        axios.get(API2, {
+            // headers: {
+            //     'Accept': 'application/json',
+            //     'Content-Type': 'application/json',
+            // },
             params: {
                 q: this.state.inputValue
             }
         })
         .then((response) => {
             console.log(response);
-            let urls = response.results.map(({ url }) => url);
-            let photos = []
-            for (let url in urls) {
-                photos.push({src: url});
-            }
-            this.setState({photos: photos});
+            let urls = response.data.results.map(({ url }) => ({'src': url}));
+            console.log(urls);
+            this.setState({photos: urls});
 
         })
         .catch(function (error) {
